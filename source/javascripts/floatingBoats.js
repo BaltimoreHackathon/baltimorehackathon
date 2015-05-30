aada5c4b4d4a0f33fe4e6fc14e33591124b09f23
ClickClickkOnSal <s.hernandez5400@gmail.com>
2015-05-30 18:11:31 -0400

    changed title and year to be images, restyled some elements

--- source/javascripts/floatingBoats.js
+++ source/javascripts/floatingBoats.js
@@ -1,8 +1,10 @@
-var rate = 50;
-var wWidth, wHeight;
-var cloud_files = [ 'pirate_ship.png', 'wave.png', 'pirate_ship.png', 'wave.png' ];
-var files = ['dirigible.png', 'airship-boat.png'];
-var floaters = {}, floatCount = 0;
+var rate    = 50,
+cloud_files = [ 'pirate_ship.png', 'wave.png', 'pirate_ship.png', 'wave.png' ],
+files       = ['airship-boat.png'],
+floaters    = {}, 
+floatCount = 0,
+wWidth, 
+wHeight;
 
 function animate() {
   var values = _.values(floaters);
@@ -14,80 +16,62 @@ function animate() {
 $(window).load(function() {
   iHeight = 0;
   iWidth = window.innerWidth;
-  for(var i=50; i > 0; i--)
+  for( var i = 50; i > 0; i-- )
     clouds();
-  for(var i=5; i > 0; i--)
+  for( var  i= 5; i > 0; i-- )
     dirigibles();
   animate();
 });
 
-function create_floater(file, klass, x, y, delta)
-{
-  var ratio = Math.random();
-  var spd = Math.ceil(ratio * delta);
-  var amp = 5 * Math.random() + .1;
-  var shift = 2 * Math.PI * Math.random();
+function clouds() {
+  var idx = Math.floor( Math.random() * cloud_files.length ),
+  height  = 200,
+  y       = iHeight + Math.floor( Math.random() * height ),
+  x       = Math.floor( Math.random() * iWidth );
+
+  create_floater(cloud_files[idx], 'cloud', x, y, 1);
+}
+
+function create_floater( file, className, x, y, delta ) {
+  var ratio = Math.random(),
+  speed     = Math.ceil(ratio * delta),
+  amplitude = 5 * Math.random() + .1,
+  shift     = 2 * Math.PI * Math.random();
 
   var image = $('<img/>', {
     src: 'images/' + file,
     style: 'width: ' + Math.floor(ratio * 100) + '%;'
   }).appendTo($('<div/>', {
-    'class': klass,
+    'class': className,
     style: 'position: absolute; z-index: 100; opacity: ' + ratio + '; left: ' + x + 'px; top: ' + y + 'px; width: 60px; height: 60px;'
   }).appendTo('#boats'));
 
   var elem = image.parent(),
-      elem_id = 'floater-' + floatCount++;
+  elem_id  = 'floater-' + floatCount++;
+
   elem.attr('id', elem_id);
   floaters[elem_id] = {
     element: elem,
-    speed: spd,
-    amplitude: amp,
+    speed: speed,
+    amplitude: amplitude,
     shift: shift,
     animate: true,
     x: x,
     y: y
   };
-
   return elem;
 }
 
-function clouds()
-{
-  var idx = Math.floor(Math.random()*cloud_files.length);
-  var height = 200;
-  var y = iHeight + Math.floor(Math.random()*height);
-  var x = Math.floor(Math.random()*iWidth);
-
-  create_floater(cloud_files[idx], 'cloud', x, y, 1);
-}
-
-function dirigibles()
-{
-  var idx = Math.floor(Math.random()*files.length);
-  var height = 200;
-  var y = iHeight + Math.floor(Math.random()*height);
-  var x = Math.floor(Math.random()*iWidth);
-
-  var dirig = create_floater(files[idx], 'dirig', x, y, 2);
-  var image = dirig.find('img').first();
+function dirigibles() {
+  var idx = Math.floor( Math.random() * files.length );
+  height  = 200,
+  y       = iHeight + Math.floor( Math.random() * height ),
+  x       = Math.floor( Math.random() * iWidth),
+  dirig   = create_floater( files[idx], 'dirig', x, y, 2 ),
+  image   = dirig.find( 'img' ).first();
 
   dirig.css('width', image.clientWidth);
   dirig.css('height', image.clientHeight);
-  dirig.click(function() {
-    var floater = floaters[$(this).attr('id')];
-    floater.animate = false;
-    loop(floater);
-  });
-}
-
-function loop(floater)
-{
-  floater.element.addClass('loop');
-  setTimeout(function() {
-    floater.element.removeClass('loop');
-    floater.animate = true;
-  }, 1000);
 }
 
 function updateFloater(floater) {
@@ -98,20 +82,16 @@ function updateFloater(floater) {
 }
 
 // reset after passing right boundary
-function move_right(floater)
-{
-  if(floater.x < window.innerWidth + 75) {
+function move_right( floater ) {
+  if( floater.x < window.innerWidth - 75 ) {
     floater.x += floater.speed;
   } else {
     floater.x = -75;
   }
-  floater.element.css('left', floater.x + 'px');
+  floater.element.css( 'left', floater.x + 'px' );
 }
 
-function bob(floater)
-{
+function bob( floater ) {
   var feq = 0.1;
-  floater.element.css('top', floater.y +
-                             floater.amplitude * Math.sin(feq * floater.x + floater.shift) +
-                             'px');
+  floater.element.css( 'top', floater.y + floater.amplitude * Math.sin( feq * floater.x + floater.shift ) + 'px' );
 }
